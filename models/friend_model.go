c8a874d5d8c202e85b6df3c95a5baf44229cb212
Chatuporn <minza1131@outlook.com>
2025-03-23 04:36:56 +0700

    Friend status not null

--- models/friend_model.go
+++ models/friend_model.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Friend struct {
-	ID        uint   `gorm:"primaryKey"`
-	SubID1    string `gorm:"uniqueIndex:idx_friend_unique"`
-	SubID2    string `gorm:"uniqueIndex:idx_friend_unique"`
-	Status    string
+	ID        uint      `gorm:"primaryKey"`
+	SubID1    string    `gorm:"uniqueIndex:idx_friend_unique"`
+	SubID2    string    `gorm:"uniqueIndex:idx_friend_unique"`
+	Status    string    `gorm:"not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
