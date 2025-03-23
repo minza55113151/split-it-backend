5cbaf01dbfab098184ee62e73c9df75cf68a2f34
Chatuporn <minza1131@outlook.com>
2025-03-24 01:50:26 +0700

    Connect DB from connection

--- db.go
+++ db.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"split-it/models"
@@ -17,23 +16,16 @@ func initDB() *gorm.DB {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_SSLMODE"),
-	)
+	dsn := os.Getenv("DATABASE_URL")
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
 
-	db.Migrator().DropTable(&models.User{})
-	db.Migrator().DropTable(&models.Friend{})
-	db.Migrator().DropTable(&models.Expense{})
+	// db.Migrator().DropTable(&models.User{})
+	// db.Migrator().DropTable(&models.Friend{})
+	// db.Migrator().DropTable(&models.Expense{})
 
 	db.AutoMigrate(&models.User{})
 	db.AutoMigrate(&models.Friend{})
